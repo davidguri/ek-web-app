b233b5a0d5dd40345bc8d6008752a7745e763497
David Guri <Davidguri@yahoo.com>
2022-05-02 12:16:29 +0200

    styling sidebar + main content

--- pages/index.js
+++ pages/index.js
@@ -20,14 +20,50 @@ export default function Home() {
             />
             <p className={styles.name}>[account]</p>
           </div>
-          <div className={styles.tabs}>
-            <div className={styles.tabsTop}>Hi</div>
-            <div className={styles.tabsBottom}>Hey</div>
-          </div>
+          <section className={styles.tabs}>
+            <section className={styles.tabsTop}>
+              <div className={styles.tabButton}>
+                <ion-icon name="person-circle" className={styles.tabIcon} />
+                <p className={styles.buttonText}>Account Info</p>
+              </div>
+              <div className={styles.tabButton}>
+                <ion-icon name="calendar" className={styles.tabIcon} />
+                <p className={styles.buttonText}>Schedule</p>
+              </div>
+              <div className={styles.tabButton}>
+                <ion-icon name="albums" className={styles.tabIcon} />
+                <p className={styles.buttonText}>Classes</p>
+              </div>
+              <div className={styles.tabButton}>
+                <ion-icon name="clipboard" className={styles.tabIcon} />
+                <p className={styles.buttonText}>Lesson Plans</p>
+              </div>
+              <div className={styles.tabButton}>
+                <ion-icon name="logo-windows" className={styles.tabIcon} />
+                <p className={styles.buttonText}>Teams</p>
+              </div>
+            </section>
+            <section className={styles.tabsBottom}>
+              <div className={styles.tabButton}>
+                <ion-icon name="log-out" className={styles.tabIcon} />
+                <p className={styles.buttonText}>Log Out</p>
+              </div>
+            </section>
+          </section>
         </section>
 
         <section className={styles.mainContent}>
-          <section className={styles.mainLeft}>Hello</section>
+          <section className={styles.mainLeft}>
+            <div className={styles.timeContainer}>
+              <div className={styles.time}>
+                <p id="time-text">TI:ME</p>
+              </div>
+              <div className={styles.additionalInfo}>
+                <p id="period">period</p>
+                <p id="date">date</p>
+              </div>
+            </div>
+          </section>
           <section className={styles.mainRight}>Hallo</section>
         </section>
       </main>
@@ -45,9 +81,16 @@ export default function Home() {
         * {
           box-sizing: border-box;
           color: white;
-          background-color: #212b38;
+          background-color: #121921;
+        }
+
+        ion-icon {
+          background: transparent;
+          font-size: 26px;
+          margin-left: 10px;
         }
       `}</style>
+      <script src="https://unpkg.com/ionicons@5.0.0/dist/ionicons.js"></script>
     </div>
   );
 }
